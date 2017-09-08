2b4e8966d7d8093502ce3b552e5cb1382d0046a2
sakachin2 <sakachin2@yahoo.co.jp>
2017-09-08 17:00:34 +0900

    V119

--- Axe/app/build/generated/source/buildConfig/release/com/xe/Axe/BuildConfig.java
+++ Axe/app/build/generated/source/buildConfig/release/com/xe/Axe/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.xe.Axe";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 118;
-  public static final String VERSION_NAME = "1.18";
+  public static final int VERSION_CODE = 119;
+  public static final String VERSION_NAME = "1.19";
 }
